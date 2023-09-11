558e6ea51cd84b8668cb0528237498b2985d905e
Daniel Truong <aduy1122@gmail.com>
2023-09-11 16:19:49 -0700

    Dev branch

--- myclubboard/src/pages/index.js
+++ myclubboard/src/pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <h1> Dev Branch </h1>
       MyClubBoard
     </main>
   );
