a08feac857175aa3f14b319acf44b3db74f1e10b
Daniel Truong <aduy1122@gmail.com>
2023-10-08 03:32:27 -0700

    connection pool

--- myclubboard/backend/src/modules/universities/university.service.ts
+++ myclubboard/backend/src/modules/universities/university.service.ts
@@ -2,11 +2,11 @@ import { NotFoundError } from 'Elysia';
 const mysql = require('mysql2/promise')
 
 // Create the connection to the database
-const connection = await mysql.createConnection(process.env.DATABASE_URL)
 export default class UniversityService {
     async getAllUni(
         limit?: number
     ) {
+        const connection = await mysql.createConnection(process.env.DATABASE_URL)
         const res = await connection.execute('select * from university')
         await connection.end();
         return res;
