65c53586b49cdf4ae931e0de67b0020b41fd9a26
Daniel Truong <aduy1122@gmail.com>
2023-11-02 17:34:28 -0700

    wip posting page

--- myclubboard/src/components/Posting Page/Postings.jsx
+++ myclubboard/src/components/Posting Page/Postings.jsx
@@ -1,14 +1,23 @@
 import { useRouter } from "next/router";
 import React from "react";
 import { AiOutlineHome } from "react-icons/ai";
+import { BiCalendar } from "react-icons/bi";
 
 const Postings = ({ postings }) => {
   return (
     <section className="w-full">
       <div>
-        {postings.map((posting) => {
-          return <Posting key={posting.id} posting={posting} />;
-        })}
+        {postings.error ? (
+          <div className="text-center text-2xl font-medium mt-8">
+            No postings found
+          </div>
+        ) : (
+          <>
+            {postings?.map((posting) => {
+              return <Posting key={posting.id} posting={posting} />;
+            })}
+          </>
+        )}
       </div>
     </section>
   );
@@ -18,6 +27,13 @@ export default Postings;
 
 const Posting = ({ posting }) => {
   const router = useRouter();
+  const date = new Date(posting.posted_date);
+  const option = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
+  const formattedDate = date.toLocaleDateString("en-US", option);
   return (
     <div
       onClick={() => router.push(`/posting/${posting.id}`)}
@@ -31,12 +47,18 @@ const Posting = ({ posting }) => {
       <div className="flex flex-col w-full">
         <p className="font-medium">{posting.title}</p>
         <div className="flex items-center mt-2">
+          <BiCalendar
+            size={20}
+            className="mr-[0.25rem]"
+            color="#115D9E"
+          />
+          <p>{formattedDate}</p>
           <AiOutlineHome
             size={20}
             className="mr-[0.25rem] ml-auto"
             color="#115D9E"
           />
-          <p className="">{posting.club_name} </p>
+          <p className="mr-2">{posting.club_name} </p>
         </div>
       </div>
     </div>
